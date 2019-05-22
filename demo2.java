b62156c81d17324a8a7976b7fbd0dcf5d271d4e3
premishu <premveer786@gmail.com>
2019-05-22 15:31:01 +0530

    first commit

---
+++ demo2.java
@@ -0,0 +1,10 @@
+public class demo2 
+	{
+public static void main(String[] args) 
+	{
+        System.out.println("This is my Second Demo program");
+	System.out.println("Program Runs Successfully");
+
+    	}
+	}
+ 
